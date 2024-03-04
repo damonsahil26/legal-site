605fb07e4c4705b0df664f200b556f0e84c9160e
Sahil Malhotra <sahil061626@gmail.com>
2024-03-04 20:43:27 +0530

    Added Why Choose Page

---
+++ src/Components/WhyChoose.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import './css/WhyChoose.css';
+
+const WhyChoose = () => {
+    return (
+        <div className="MuiContainer-root MuiContainer-maxWidthLg mui-1qsxih2">
+            <div className="MuiBox-root mui-3thk76">
+                <h2 className="MuiTypography-root MuiTypography-h4 mui-1st9khg">
+                    Why choose Alex?
+                </h2>
+                <div className="MuiBox-root mui-1vmhh01">
+                    <div className="MuiBox-root mui-1ebprri">
+                        <div className="MuiBox-root mui-1mfn5mn">
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mui-vubbuv"
+                                focusable="false"
+                                color="#83B9C0"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="MouseOutlinedIcon"
+                            >
+                                <path d="M20 9c-.04-4.39-3.6-7.93-8-7.93S4.04 4.61 4 9v6c0 4.42 3.58 8 8 8s8-3.58 8-8zm-2 0h-5V3.16c2.81.47 4.96 2.9 5 5.84m-7-5.84V9H6c.04-2.94 2.19-5.37 5-5.84M18 15c0 3.31-2.69 6-6 6s-6-2.69-6-6v-4h12z"></path>
+                            </svg>
+                            <div className="MuiBox-root mui-n085mf">
+                                <h6 className="MuiTypography-root MuiTypography-h6 mui-170ssnn">
+                                    <span>Simplicity</span>
+                                </h6>
+                                <p className="MuiTypography-root MuiTypography-body2 mui-1xx2ofw">
+                                    <span>Create or add your company in a few clicks</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="MuiBox-root mui-1ebprri">
+                        <div className="MuiBox-root mui-1mfn5mn">
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mui-vubbuv"
+                                focusable="false"
+                                color="#83B9C0"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="DescriptionIcon"
+                            >
+                                <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8zm2 16H8v-2h8zm0-4H8v-2h8zm-3-5V3.5L18.5 9z"></path>
+                            </svg>
+                            <div className="MuiBox-root mui-n085mf">
+                                <h6 className="MuiTypography-root MuiTypography-h6 mui-170ssnn">
+                                    <span>Quality</span>
+                                </h6>
+                                <p className="MuiTypography-root MuiTypography-body2 mui-1xx2ofw">
+                                    <span>All our documents have been designed by lawyers</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="MuiBox-root mui-1ebprri">
+                        <div className="MuiBox-root mui-1mfn5mn">
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mui-vubbuv"
+                                focusable="false"
+                                color="#83B9C0"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="ContactsOutlinedIcon"
+                            >
+                                <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 14H4V6h16zM4 0h16v2H4zm0 22h16v2H4zm8-10c1.38 0 2.5-1.12 2.5-2.5S13.38 7 12 7 9.5 8.12 9.5 9.5 10.62 12 12 12m0-3.5c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1m5 7.49C17 13.9 13.69 13 12 13s-5 .9-5 2.99V17h10zm-8.19-.49c.61-.52 2.03-1 3.19-1 1.17 0 2.59.48 3.2 1z"></path>
+                            </svg>
+                            <div className="MuiBox-root mui-n085mf">
+                                <h6 className="MuiTypography-root MuiTypography-h6 mui-170ssnn">
+                                    <span>Follow up</span>
+                                </h6>
+                                <p className="MuiTypography-root MuiTypography-body2 mui-1xx2ofw">
+                                    <span>Benefit from a dedicated contact at any time</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="MuiBox-root mui-1ebprri">
+                        <div className="MuiBox-root mui-1mfn5mn">
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mui-vubbuv"
+                                focusable="false"
+                                color="#83B9C0"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="CheckCircleOutlinedIcon"
+                            >
+                                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z"></path>
+                            </svg>
+                            <div className="MuiBox-root mui-n085mf">
+                                <h6 className="MuiTypography-root MuiTypography-h6 mui-170ssnn">
+                                    <span>Compliance</span>
+                                </h6>
+                                <p className="MuiTypography-root MuiTypography-body2 mui-1xx2ofw">
+                                    <span>Your legal documentation always remains well kept and within reach</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="MuiBox-root mui-1ebprri">
+                        <div className="MuiBox-root mui-1mfn5mn">
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mui-vubbuv"
+                                focusable="false"
+                                color="#83B9C0"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="EuroIcon"
+                            >
+                                <path d="M15 18.5c-2.51 0-4.68-1.42-5.76-3.5H15l1-2H8.58c-.05-.33-.08-.66-.08-1s.03-.67.08-1H15l1-2H9.24C10.32 6.92 12.5 5.5 15 5.5c1.61 0 3.09.59 4.23 1.57L21 5.3C19.41 3.87 17.3 3 15 3c-3.92 0-7.24 2.51-8.48 6H3l-1 2h4.06c-.04.33-.06.66-.06 1s.02.67.06 1H3l-1 2h4.52c1.24 3.49 4.56 6 8.48 6 2.31 0 4.41-.87 6-2.3l-1.78-1.77c-1.13.98-2.6 1.57-4.22 1.57"></path>
+                            </svg>
+                            <div className="MuiBox-root mui-n085mf">
+                                <h6 className="MuiTypography-root MuiTypography-h6 mui-170ssnn">
+                                    <span>Price</span>
+                                </h6>
+                                <p className="MuiTypography-root MuiTypography-body2 mui-1xx2ofw">
+                                    <span>Reduce your legal fees, consult our prices</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="MuiBox-root mui-1ebprri">
+                        <div className="MuiBox-root mui-1mfn5mn">
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mui-vubbuv"
+                                focusable="false"
+                                color="#83B9C0"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="LockOutlinedIcon"
+                            >
+                                <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2M9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9zm9 14H6V10h12zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2"></path>
+                            </svg>
+                            <div className="MuiBox-root mui-n085mf">
+                                <h6 className="MuiTypography-root MuiTypography-h6 mui-170ssnn">
+                                    <span>Security</span>
+                                </h6>
+                                <p className="MuiTypography-root MuiTypography-body2 mui-1xx2ofw">
+                                    <span>Your documentation is available on our secure platform</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="MuiBox-root mui-90nejt">
+                    <h6 className="MuiTypography-root MuiTypography-h6 mui-11bdqlm">
+                        Because poorly maintained legal documentation can be a source of later problems for entrepreneurs, with Alex you have{' '}
+                        <strong className="mui-11um2kn">the assurance of being compliant</strong> and your company is prepared for your future needs.
+                    </h6>
+                </div>
+                <div className="MuiBox-root mui-yg2p1p">
+                    <a
+                        className="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium mui-ha489"
+                        tabIndex="0"
+                        href="/creation-societe"                    >
+                        <font style={{ VerticalAlign: "inherit" }}><font style={{ VerticalAlign: "inherit" }}>I create my company</font></font>
+                        <span className="MuiTouchRipple-root mui-w0pj6f"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhyChoose;
